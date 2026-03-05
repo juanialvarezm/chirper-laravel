cd262b134704effd732548269793a4032b58392e
Juani <juanialvarezzm@gmail.com>
2026-03-05 14:14:24 -0300

    finished V0.1 chirper

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class ChirpController extends Controller
-    {
+
+{
+    use AuthorizesRequests;
+    
     public function  index(){
       $chirps = Chirp::with("user")
       ->latest()
@@ -29,7 +33,20 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "message"=>"required|string|max:255"
+        ],[
+            "message.required"=>"Please write something!",
+            "message.max"=>"Maximum of 255 characters."
+        ]);
+
+        Chirp::create([
+            "message"=>$validated["message"]
+        ]);
+
+        auth()->user()->chirps()->create($validated);
+
+        return redirect("/")->with("success", "Chirp Created");
     }
 
     /**
@@ -43,24 +60,39 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Chirp $chirp)
     {
-        //
+        $this->authorize("update", $chirp);
+        return view("chirps.edit",compact("chirp"));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Chirp $chirp)
     {
-        //
+        $this->authorize("update", $chirp);
+          $validated = $request->validate([
+            "message"=>"required|string|max:255"
+        ],[
+            "message.required"=>"Please write something!",
+            "message.max"=>"Maximum of 255 characters."
+        ]);
+
+        $chirp->update($validated);
+        $this->authorize("update", $chirp);
+
+
+        return redirect("/")->with("success", "Your chirp has been updated");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Chirp $chirp)
     {
-        //
+        $chirp->delete();
+
+        return redirect("/")->with("success", "Your chirp has been deleted");
     }
 }
